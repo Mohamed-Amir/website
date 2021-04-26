bd42305bfb519711d6209f665d62465553d5d7bb
Mohamed-Amir <mohamed.amir2233@gmail.com>
2021-04-26 21:43:27 +0200

    count

--- resources/views/Admin/includes/homeAdmin/counts.blade.php
+++ resources/views/Admin/includes/homeAdmin/counts.blade.php
@@ -1,84 +1,66 @@
-{{--<div class="card-group">--}}
-{{--    <!-- Card -->--}}
-{{--    <div class="card">--}}
-{{--        <div class="card-body">--}}
-{{--            <div class="d-flex align-items-center">--}}
-{{--                <div class="m-r-10">--}}
-{{--                                    <span class="btn btn-circle btn-lg bg-danger">--}}
-{{--                                        <i class="ti-clipboard text-white"></i>--}}
-{{--                                    </span>--}}
-{{--                </div>--}}
-{{--                <div>--}}
-{{--                    عدد البرامج--}}
-{{--                </div>--}}
-{{--                <div class="ml-auto">--}}
-{{--                    <h2 class="m-b-0 font-light">{{\App\Models\Programs::count()}}</h2>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <!-- Card -->--}}
-{{--    <!-- Card -->--}}
-{{--    <div class="card">--}}
-{{--        <div class="card-body">--}}
-{{--            <div class="d-flex align-items-center">--}}
-{{--                <div class="m-r-10">--}}
-{{--                                    <span class="btn btn-circle btn-lg btn-info">--}}
-{{--                                        <i class=" icon-Add-User text-white"></i>--}}
-{{--                                    </span>--}}
-{{--                </div>--}}
-{{--                <div>--}}
-{{--                    عدد الاقسام--}}
+<div class="card-group">
+    <!-- Card -->
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <div class="m-r-10">
+                                    <span class="btn btn-circle btn-lg bg-danger">
+                                        <i class="ti-clipboard text-white"></i>
+                                    </span>
+                </div>
+                <div>
+                    عدد الخدمات
+                </div>
+                <div class="ml-auto">
+                    <h2 class="m-b-0 font-light">{{\App\Models\Services::count()}}</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Card -->
+    <!-- Card -->
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <div class="m-r-10">
+                                    <span class="btn btn-circle btn-lg btn-info">
+                                        <i class=" icon-Add-User text-white"></i>
+                                    </span>
+                </div>
+                <div>
+                    عدد الاستشارات
 
-{{--                </div>--}}
-{{--                <div class="ml-auto">--}}
-{{--                    <h2 class="m-b-0 font-light">{{\App\Models\Sections::count()}}</h2>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <!-- Card -->--}}
-{{--    <!-- Card -->--}}
-{{--    <div class="card">--}}
-{{--        <div class="card-body">--}}
-{{--            <div class="d-flex align-items-center">--}}
-{{--                <div class="m-r-10">--}}
-{{--                                    <span class="btn btn-circle btn-lg bg-success">--}}
-{{--                                        <i class="icon-Add-UserStar text-white"></i>--}}
-{{--                                    </span>--}}
-{{--                </div>--}}
-{{--                <div>--}}
-{{--                    عدد الصور--}}
+                </div>
+                <div class="ml-auto">
+                    <h2 class="m-b-0 font-light">{{\App\Models\Consults::count()}}</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Card -->
+    <!-- Card -->
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <div class="m-r-10">
+                                    <span class="btn btn-circle btn-lg bg-success">
+                                        <i class="icon-Add-UserStar text-white"></i>
+                                    </span>
+                </div>
+                <div>
+                    عدد الرسائل
 
-{{--                </div>--}}
-{{--                <div class="ml-auto">--}}
-{{--                    <h2 class="m-b-0 font-light">{{\App\Models\Gallery::count()}}</h2>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <!-- Card -->--}}
-{{--    <!-- Card -->--}}
-{{--    <div class="card">--}}
-{{--        <div class="card-body">--}}
-{{--            <div class="d-flex align-items-center">--}}
-{{--                <div class="m-r-10">--}}
-{{--                                    <span class="btn btn-circle btn-lg bg-warning">--}}
-{{--                                        <i class="icon-Address-Book2 text-white"></i>--}}
-{{--                                    </span>--}}
-{{--                </div>--}}
-{{--                <div>--}}
-{{--                    عدد فيدوهات البرامج--}}
-
-{{--                </div>--}}
-{{--                <div class="ml-auto">--}}
-{{--                    <h2 class="m-b-0 font-light">{{\App\Models\Program_videos::count()}}</h2>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <!-- Card -->--}}
-{{--    <!-- Column -->--}}
+                </div>
+                <div class="ml-auto">
+                    <h2 class="m-b-0 font-light">{{\App\Models\Contact_us::count()}}</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Card -->
+    <!-- Card -->
+    <!-- Card -->
+    <!-- Column -->
 
 
-{{--</div>--}}
+</div>
